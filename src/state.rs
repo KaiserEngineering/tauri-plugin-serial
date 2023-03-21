44d9db6a3cbdc3b649336a384fb362abe38a59ab
Craig Kaiser <craig@kaiserengineering.io>
2023-03-21 12:10:53 -0400

    t

--- src/state.rs
+++ src/state.rs
@@ -26,25 +26,7 @@ pub struct ReadData<'a> {
 }
 
 impl SerialState {
-    pub async fn validate_connection(
-        serial_state: State<'_, SerialState>,
-    ) -> Result<String, String> {
-        // Wait for our connection
-        let serial_connection = serial_state.connection.lock().await;
-
-        let port_name = state_copy.port.lock().await.clone();
-        if !serial_connection.is_some() || !usb_is_mounted(port_name) {
-            let state_copy = serial_state.clone();
-
-            connect(port_name.to_string(), state_copy).await?;
-
-            return Ok("New session is good".to_string());
-        }
-
-        Ok("Old session is good".to_string())
-    }
-
-    fn usb_is_mounted(port_name: &str) -> bool {
+    pub fn usb_is_mounted(port_name: &str) -> bool {
         println!("Serial Info is: {:?}", port_name);
         for device in rusb::devices().unwrap().iter() {
             let device_desc = device.device_descriptor().unwrap();
@@ -58,5 +40,24 @@ impl SerialState {
             );
         }
         true
+    }
+
+    pub async fn validate_connection(
+        self,
+        serial_state: State<'_, SerialState>,
+    ) -> Result<String, String> {
+        // Wait for our connection
+        let serial_connection = serial_state.connection.lock().await;
+
+        let port_name = state_copy.port.lock().await.clone();
+        if !serial_connection.is_some() || self.usb_is_mounted(port_name) {
+            let state_copy = serial_state.clone();
+
+            connect(port_name.to_string(), state_copy).await?;
+
+            return Ok("New session is good".to_string());
+        }
+
+        Ok("Old session is good".to_string())
     }
 }
