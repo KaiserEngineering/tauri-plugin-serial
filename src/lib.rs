929ccbed27fb9cd67a7adf4119394472f5030331
Craig Kaiser <craig@kaiserengineering.io>
2023-03-21 11:22:39 -0400

    t

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use tauri::{
-    plugin::{Builder as PluginBuilder, TauriPlugin},
+    plugin::{Builder, TauriPlugin},
     Manager, Runtime,
 };
 
@@ -10,48 +10,24 @@ use crate::command::{connect, dtr, find_available_ports, get_connection, write};
 pub mod command;
 pub mod state;
 
-pub struct Builder {
-    baud_rate: u32,
-}
-
-impl Default for Builder {
-    fn default() -> Self {
-        Self { baud_rate: 57600 }
-    }
-}
-
-impl Builder {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn baud_rate(mut self, baud_rate: u32) -> Self {
-        self.baud_rate = baud_rate;
-        self
-    }
-
-    pub fn build<R: Runtime>(self) -> TauriPlugin<R> {
-        PluginBuilder::new("serial")
-            .invoke_handler(tauri::generate_handler![
-                find_available_ports,
-                get_connection,
-                connect,
-                write,
-                dtr
-            ])
-            .setup(move |app_handle, api| {
-                println!("Config is {:?}", api.config());
-                app_handle.manage(SerialState {
-                    port: Default::default(),
-                    connection: Default::default(),
-                    baud_rate: self.baud_rate,
-                });
-                Ok(())
-            })
-            .build()
-    }
-}
-
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    Builder::new().build()
+    Builder::new("serial")
+        .invoke_handler(tauri::generate_handler![
+            find_available_ports,
+            get_connection,
+            connect,
+            write,
+            dtr
+        ])
+        .setup(move |app_handle, api| {
+            println!("{:?}", api.config());
+
+            app_handle.manage(SerialState {
+                port: Default::default(),
+                connection: Default::default(),
+                baud_rate: 5700,
+            });
+            Ok(())
+        })
+        .build()
 }
